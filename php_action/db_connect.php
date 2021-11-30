c813bbf5cad9bbdb464a2f3f38c252e2c0294811
Luana-Cristina <luanacristina200@gmail.com>
2021-11-29 21:12:37 -0300

    Conexão com o Banco de dados

---
+++ php_action/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+    //conexão com o banco de dados
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+    $db_name = 'atp_web';
+
+    $connect = mysqli_connect($servername, $username, $password, $db_name);
+
+    if(mysqli_connect_error()):
+        echo "Erro na conexão: ". mysqli_connect_error();
+    endif;
+?>
